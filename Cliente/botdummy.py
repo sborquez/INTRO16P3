3615a5307abcb6f4ed03b9f4bf4524c08dc42262
gaspar octavio correa vergara <gcorrea@merced.labs.inf.utfsm.cl>
2016-08-18 12:14:38 -0300

    Changes to be committed: 	modified:   botdummy.py  Changes not staged for commit: 	modified:   cliente.py

---
+++ Cliente/botdummy.py
@@ -0,0 +1,44 @@
+import numpy
+import random
+
+def escoger ( accion ):
+    if ( accion == 3 ):
+        l = [-1,1]
+        return random.choice(list(l))
+    elif( accion == 1 ):
+        l = [-5,-4,4,5]
+        return random.choice(list(l))
+    elif( accion == 2 ):
+        l = [-3,-2,2,3]
+        return random.choice(list(l))
+
+def periodico ( r ):
+    if ( r >= n ): return (n-r)
+    return r
+
+def mover ( posicion, battlefield ):
+    
+    delta = escoger(3)
+    battlefield[ posicion[0], posicion[1] ] = 0
+    
+    if ( random.randint(0,1) ):
+        posicion = posicion[0],periodico(posicion[1]+delta)
+        battlefield[ posicion[0], posicion[1] ] = 1
+        return posicion,battlefield
+        
+    posicion = periodico( posicion[0]+1 ), posicion[1]
+    battlefield[ posicion[0], posicion[1] ] = 1
+    return posicion,battlefield
+
+def disparar ( amenaza, posicion ):
+    delta = escoger( amenaza )
+    if( random.randint(0,1) ):
+        return posicion[0]+delta
+    return posicion[1]+delta
+
+#def estimar_amenaza ( posicion,battlefield ):
+ #   for i in xrange(2):
+  #      for j in [-5,-4,-3,-2,-1,1,2,3,4,5]:
+   #         if  battlefieldperiodico ( posicion[i]+j )
+
+
