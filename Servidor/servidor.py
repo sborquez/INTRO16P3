2438fc02793f8a37b3cf9ce1bbc4553dd97fc352
Gaspar Correa <gcorrea@fry.csrg.inf.utfsm.cl>
2016-08-31 12:34:21 -0300

    Changes to be committed: 	modified:   servidor.py

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,10 +1,22 @@
 import socket
 import dummy_bot
+import threading
+
+def conectar():
+    conexiones_entrantes = list()
+    while( len (conexiones_entrantes) < 3 ):
+        conexiones_entrantes.append( server.accept() )
+        print "Conexion exitosa!"
+    print "Todos se han conectado"
+    return conexiones_entrantes
+
 
 servidor = socket.socket()
-servidor.bind( ( "10.10.9.13",6969 ) )
-servidor.listen(1)
-socket_cliente, datos_cliente = servidor.accept()
+servidor.bind( (' ',0 ) )
+print servidor.getsockname()
+servidor.listen(2)
+print "Esperando Conexiones"
+conexiones_entrantes = conectar()
 servidor.close()
 
 # iniciar juego
