5a411fe6c9222ae34edd5780b53775184d753f02
Gaspar Correa <gasparcorreavergara@gmail.com>
2016-09-27 19:59:18 -0300

    Añadiendo nuevas reglas

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -115,14 +115,14 @@ while ( juego ):
         print "Esperando accion de ", jugador
         mensaje_recibido = socket_o.recv(1024)
         
-        disparo = map(int,mensaje_recibido.split("-")[0].split(","))
+        disparo = map(int,(mensaje_recibido.split("-")[0]).split(","))
         if not validar_disparo( disparo ):
             battefield[x0][y0] = 0
             matar(conexiones_entrantes,stats, id, jugador)
             print jugador," ha cometido disparo fuera de rango"
 
         #evaluar_disparo( battlefield, disparo)
-        movimiento = map(int,mensaje_recibido.split("-")[1].split(","))
+        movimiento = map(int,(mensaje_recibido.split("/")[1]).split(","))
         if not validar_movimiento( movimiento ):
             matar(conexiones_entrante, stats, id , jugador)
             battefield[x0][y0] = 0
@@ -160,6 +160,9 @@ while ( juego ):
             stats[id_golpeado][1]-=1
         
         for id in stats:
+            if ( stats[id][2] == 0 ):
+                stats[id][1] = stats[id][1]-1;
+                stats[id][2] = 3;
             if ( stats[id][1] == 0 ):
                 print "Ha muerto ", stats[id][0]
                 matar(id)
