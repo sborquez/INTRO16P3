4dbe838fce2dfdbd7d8cad698e2f2267352a883d
sborquez <sebastian.borquez.g@gmail.com>
2016-09-28 00:26:58 -0300

    fixed: formato logs, titulo y aparece:id despues de juego:comenzar

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -85,7 +85,7 @@ def spawn_all( battlefield , conexiones_entrantes, log):
 """
 
 log = list()
-title = time.strftime("%c")
+title = time.strftime("%c").remplace(" ","_")
 log.append("#TITLE;"+title+"/"+str(SIZE))
 battlefield = numpy.tile(0,(20,20))
 servidor = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
@@ -94,63 +94,65 @@ print servidor.getsockname()
 servidor.listen(NJ)
 print "Esperando Conexiones"
 conexiones_entrantes = conectar(servidor, log)
+log.append("juego:comenzar")
 stats = spawn_all( battlefield, conexiones_entrantes, log)
 print stats, "\n-------\n", conexiones_entrantes,"\n--------\n",battlefield
 juego = 1
-log.append("juego:comenzar")
-while ( juego ):
-    if ( len(conexiones_entrantes) == 0):
-        break 
-    for id in conexiones_entrantes:
-        jugador = conexiones_entrantes[id][2]
-        socket_o = conexiones_entrantes[id][0]
-        log.append("alertar:"+jugador)
-        print "Alertando a ", jugador
-        posicion = stats[id][3]
-        socket_o.send( estimar_amenaza(posicion, battlefield, SIZE) )
-        print "Esperando accion de ", jugador
-        mensaje_recibido = socket_o.recv(1024)
-        print mensaje_recibido
-        disparo = (mensaje_recibido.split("-")[0]).split(",")
-        evaluar_disparo( battlefield, disparo)
-        posicion = (mensaje_recibido.split("-")[1]).split(",")
-        print disparo, posicion
-        posicion[0] = int(posicion[0])
-        posicion[1] = int(posicion[1])
-        disparo[0] = int(disparo[0])
-        disparo[1] = int(disparo[1])
-        estado = evaluar_disparo(battlefield, disparo)
-        if ( estado == "D"):
-            stats[battlefield[disparo[0]][disparo[1]]][1]-=1
-            id2 = stats[battlefield[disparo[0]][disparo[1]]][0]
-            stats[id][2]+=1
-            #TODO
-            log.append("disparar:{ID},{X},{Y},{ID2}".format(ID=jugador, X=disparo[0],Y=disparo[1],ID2=id2))
-            log.append("muere:{id2}".format(ID2=id2))
-        elif ( estado == "W"):
-            log.append("disparar:{ID},{X},{Y},{OBJ}".format(ID=jugador, X=disparo[0],Y=disparo[1],OBJ="None"))
-        estado = evaluar_movimiento(battlefield, posicion)
-        x,y = posicion
-        if ( estado == "M"):
-            battlefield[stats[id][3][0]][stats[id][3][1]] = 0
-            stats[id][3] = posicion
-            battlefield[posicion[0]][posicion[1]] = id
-            #TODO
-            log.append("moverse:{ID},{X},{Y}".format(ID=jugador, X=posicion[0],Y=posicion[1])) #se movio
-        elif ( estado == "C"):
-            #TODO
-            log.append("colision:{ID}".format(ID = jugador)) #se murio1
-            id2 = stats[battlefield[posicion[0]][posicion[1]]][0]
-            log.append("colision:{ID}".format(ID = id2)) #se murio2
-            del conexiones_entrantes[id]
-            del conexiones_entrantes[battlefield[posicion[0]][posicion[1]]]
-            battlefield[stats[id][3][0]][stats[id][3][1]] = 0
-            battlefield[posicion[0]][posicion[1]] = 0
-            stats[battlefield[disparo[0]][disparo[1]]][2]-=1
+try:
+    while ( juego ):
+        if ( len(conexiones_entrantes) == 0):
+            break 
+        for id in conexiones_entrantes:
+            jugador = conexiones_entrantes[id][2]
+            socket_o = conexiones_entrantes[id][0]
+            #log.append("alertar:"+jugador)
+            print "Alertando a ", jugador
+            posicion = stats[id][3]
+            socket_o.send( estimar_amenaza(posicion, battlefield, SIZE) )
+            print "Esperando accion de ", jugador
+            mensaje_recibido = socket_o.recv(1024)
+            print mensaje_recibido
+            disparo = (mensaje_recibido.split("-")[0]).split(",")
+            evaluar_disparo( battlefield, disparo)
+            posicion = (mensaje_recibido.split("-")[1]).split(",")
+            print disparo, posicion
+            posicion[0] = int(posicion[0])
+            posicion[1] = int(posicion[1])
+            disparo[0] = int(disparo[0])
+            disparo[1] = int(disparo[1])
+            estado = evaluar_disparo(battlefield, disparo)
+            if ( estado == "D"):
+                stats[battlefield[disparo[0]][disparo[1]]][1]-=1
+                id2 = stats[battlefield[disparo[0]][disparo[1]]][0]
+                stats[id][2]+=1
+                #TODO
+                log.append("disparar:{ID},{X},{Y},{ID2}".format(ID=jugador, X=disparo[0],Y=disparo[1],ID2=id2))
+                log.append("muere:{id2}".format(ID2=id2))
+            elif ( estado == "W"):
+                log.append("disparar:{ID},{X},{Y},{OBJ}".format(ID=jugador, X=disparo[0],Y=disparo[1],OBJ="None"))
+            estado = evaluar_movimiento(battlefield, posicion)
+            x,y = posicion
+            if ( estado == "M"):
+                battlefield[stats[id][3][0]][stats[id][3][1]] = 0
+                stats[id][3] = posicion
+                battlefield[posicion[0]][posicion[1]] = id
+                #TODO
+                log.append("moverse:{ID},{X},{Y}".format(ID=jugador, X=posicion[0],Y=posicion[1])) #se movio
+            elif ( estado == "C"):
+                #TODO
+                log.append("colision:{ID}".format(ID = jugador)) #se murio1
+                id2 = stats[battlefield[posicion[0]][posicion[1]]][0]
+                log.append("colision:{ID}".format(ID = id2)) #se murio2
+                del conexiones_entrantes[id]
+                del conexiones_entrantes[battlefield[posicion[0]][posicion[1]]]
+                battlefield[stats[id][3][0]][stats[id][3][1]] = 0
+                battlefield[posicion[0]][posicion[1]] = 0
+                stats[battlefield[disparo[0]][disparo[1]]][2]-=1
 
-    stats, conexiones_entrantes, log =  fin_turno(stats, conexiones_entrantes, log)
+        stats, conexiones_entrantes, log =  fin_turno(stats, conexiones_entrantes, log)
+except KeyboardInterrupt:
+    print "STOP IT!"
 
-title.replace(" ","_")
 servidor.close()
 log.append("juego:terminar")
 with open(title+".log","w") as log_file:
