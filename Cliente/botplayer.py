aaa021035b97433314dbad4960f8e2562aa2862a
Gaspar Correa <gasparcorreavergara@gmail.com>
2016-09-28 08:56:54 -0300

    Servidor falta buscar forma de eliminar elemento del dict sin interrumpir el for

--- Cliente/botplayer.py
+++ Cliente/botplayer.py
@@ -1,5 +1,4 @@
 import numpy
-import random
 
 
 def escoger_movimiento( amenazas ):
--- Cliente/cliente_dummy.py
+++ Cliente/cliente_dummy.py
@@ -1,41 +1,25 @@
 import socket
-import numpy
-import random
 from botdummy import *
-
-battlefield = numpy.tile(0,(20,20))
 
 def spawn(cliente):
     mensaje = cliente.recv(1024)
     posicion = mensaje.split(",")
     return int(posicion[0]), int(posicion[1])
 
-#LOGIN
 cliente = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 IP = raw_input("ingrese la ip ")
 PORT = input("ingrese el puerto ")
 cliente.connect( (IP ,PORT ) )
 cliente.send( "dummy" )
-
-#SPAWN INICIAL
 posicion = spawn(cliente) 
 
 juego = 1
 while (juego):
-    #RECIBIR ORDEN DEL SERVIDOR, Y EN BASE A ESO ACTUALIZAR NUESTRO ESTADO Y RESPONDER
 
     mensaje = cliente.recv(1024)
+    disparo = escoger_disparo( mensaje )
+    movimiento = escoger_movimiento()
+    mensaje = disparo + "/" + movimiento
+    cliente.send( mensaje )
 
-    #SI RECIBE UNA AMENAZA ESTAMOS OK, SE PROSIGUE CON ATACAR Y LUEGO MOVERSE
-    print mensaje
-    if ( mensaje[0] == "a"):
-        amenazas = mensaje
-    #EN BASE A LA LISTA AMENAZAS, TOMAR UNA DECISION
-    disparo_x, disparo_y = disparar( random.randint(1,len( amenazas.split("-") ) ) )
-    #ESCOGER UN MOVIMIENTO
-    posicion_x, posicion_y = mover( (posicion[0],posicion[1]) )
-    #ENVIAR DISPARO, MOVIMIENTO
-    cliente.send(str(disparo_x)+","+str(disparo_y)+"-"+str(posicion_x)+","+str(posicion_y) )
-    #IF MUERTO --> RE-SPAWN
-    #IF GAME OVER --> BREAK    
 cliente.close()
