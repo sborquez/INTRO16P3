68b0994a5ab2b88bdc50a9106cf6e2b0d34d87f9
cmoltedos <cesar.moltedo@gmail.com>
2016-10-20 15:22:12 -0300

    Script para correr multiples clientes al mismo tiempo

---
+++ exe.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Space Guars executor"
+
+echo -n "Run Server or Client [s/c]? "
+read answer
+if [ "$answer" == "s" ]; then
+	cd Servidor/
+	python servidor.py
+	clear
+elif [ "$answer" == "c" ]; then
+	cd Cliente/
+	echo -n "Dummy Bot: "
+	read dummy
+	echo -n "Smart Bot: "
+	read smart
+	echo -n "Server IP: "
+	read ip
+	echo -n "Server Port: "
+	read port
+	until [ "$dummy" = "0" ]; do
+		echo "Dummy bot n: " $dummy
+		printf "$ip\n$port\n" | python cliente_dummy.py &
+		let "dummy -= 1"
+	done
+	until [ "$smart" = "0" ]; do
+		echo "Smart bot n: " $smart
+		printf "$ip\n$port\n" | python cliente_alumnos.py &
+		let "smart -= 1"
+	done
+	exit 0
+else
+	clear
+fi
